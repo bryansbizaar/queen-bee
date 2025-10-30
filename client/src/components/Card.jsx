ba3341f431b4135e64c47d942b3159191bd695bd
Bryan <bryan.owens@developersinstitute.co.nz>
2025-10-30 23:50:28 +1300

    Use shared constants file for API_URL in Card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,15 +1,12 @@
 import PropTypes from "prop-types";
 import formatAmount from "../utils/formatAmount";
+import { API_URL } from "../constants";
 
 const Card = ({ title, price, description, image }) => {
-  // Get API URL from environment variable (set during build)
-  // This URL is baked into the bundle at build time
-  const apiUrl = import.meta.env.VITE_API_URL;
-  
   // Handle both full URLs and image filenames
   const imageUrl = image?.includes("http")
     ? image
-    : `${apiUrl}/images/${image}`;
+    : `${API_URL}/images/${image}`;
 
   return (
     <article className="card">
