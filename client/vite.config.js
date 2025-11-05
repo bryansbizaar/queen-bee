688e8f380c7745c575c15bb41a7f33569c785543
Bryan <bryan.owens@developersinstitute.co.nz>
2025-11-05 14:25:28 +1300

    feat: switch to live Stripe keys for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,8 +11,9 @@ export default defineConfig(({ mode }) => {
   // If you need to update it, get it from: https://dashboard.stripe.com/test/apikeys
   const stripeKey =
     mode === "production"
-      ? "pk_test_51KiUrjF9MElxuMjviJAW9Uv4u6GiNL3HuovBIm6VTNzJo7deHAlkLyV2I9CyQcVgeB2JsUfRNqtRpg6JJPJlK93m00n4W0mWKp"
-      : (import.meta.env?.VITE_STRIPE_PUBLISHABLE_KEY || "pk_test_51KiUrjF9MElxuMjviJAW9Uv4u6GiNL3HuovBIm6VTNzJo7deHAlkLyV2I9CyQcVgeB2JsUfRNqtRpg6JJPJlK93m00n4W0mWKp");
+      ? "pk_live_51KiUrjF9MElxuMjvO6TbY3SWGKfEyjHA3v9sbf5k6aTNsRLiQkIuDv9ihHTeQPaDb2Kr0536SeWU40Lydkcg6JNX00Blbp0Q0M"
+      : import.meta.env?.VITE_STRIPE_PUBLISHABLE_KEY ||
+        "pk_test_51KiUrjF9MElxuMjviJAW9Uv4u6GiNL3HuovBIm6VTNzJo7deHAlkLyV2I9CyQcVgeB2JsUfRNqtRpg6JJPJlK93m00n4W0mWKp";
 
   return {
     plugins: [react()],
