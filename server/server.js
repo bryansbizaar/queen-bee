bfd2f7f46481b287e6339a57d29251d7664fd5fb
Bryan <bryan.owens@developersinstitute.co.nz>
2025-11-05 13:37:17 +1300

    fix: bind server to 0.0.0.0 for Fly.io

--- server/server.js
+++ server/server.js
@@ -4,16 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = parseInt(process.env.PORT) || 8080;
-
 // Added error handling for the server
 const server = app
-  .listen(port, () => {
+  .listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
   })
   .on("error", (err) => {
     if (err.code === "EADDRINUSE") {
       console.error(`Port ${port} is already in use. Trying port ${port + 1}`);
-      server.listen(port + 1);
+      server.listen(port + 1, "0.0.0.0");
     } else {
       console.error("Server error:", err);
     }
